82df34bd3da86f82702f32b60d9926e6adadace0
Alexandre Ayres <aleayrees@gmail.com>
2026-05-13 15:58:06 -0300

    security: move secrets to config.js (gitignored)

---
+++ config.example.js
@@ -0,0 +1,5 @@
+window.HIMATER_CONFIG = {
+  SUPA_KEY: 'your_alfra_supabase_service_role_key',
+  HIMATER_SUPA_KEY: 'your_himater_supabase_service_role_key',
+  GSHEETS_API_KEY: 'your_google_sheets_api_key',
+};
